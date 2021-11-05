0b9cb84ca256c849ba65c947f7710af8353c11b5
Francisco Veiras <74626997+francisco-svg761@users.noreply.github.com>
2021-11-05 18:49:45 -0300

    control panel

--- components/notes/notes/writer/index.js
+++ components/notes/notes/writer/index.js
@@ -7,124 +7,19 @@ import { InfoNotes } from './info';
 export const Writer = () => {
   // categorie selected
 
-  const [data, setData] = useState('paragraph');
-
-  const setTitle = () => {
-    if (data === 'title') {
-      setData('paragraph');
-    } else {
-      setData('title');
-    }
-  };
-
-  const setSubtitle = () => {
-    if (data === 'subtitle') {
-      setData('paragraph');
-    } else {
-      setData('subtitle');
-    }
-  };
-
-  const setParagraph = () => {
-    if (data === 'paragraph') {
-      setData('paragraph');
-    } else {
-      setData('paragraph');
-    }
-  };
-
-  const setPhoto = () => {
-    if (data === 'photo') {
-      setData('paragraph');
-    } else {
-      setData('photo');
-    }
-  };
-
-  const setCode = () => {
-    if (data === 'code') {
-      setData('paragraph');
-    } else {
-      setData('code');
-    }
-  };
-
-  // info
-
-  const [info, setInfo] = useState([]);
-
   // salto de página
 
-  const [linesJump, setLinesJump] = useState([1]);
-
-  console.log(linesJump);
+  const [lineJump, setLinesJump] = useState([1]);
 
   return (
     <Box display="flex" maxWidth="full" flexDirection="column" gridGap={4}>
-      <Flex gridGap={4}>
-        <Box
-          onClick={setTitle}
-          background={(data === 'title' && 'purple.400') || 'purple.200'}
-          cursor="pointer"
-          px={4}
-          py={2}
-          _hover={{ background: 'purple.400' }}
-          borderRadius="9999px"
-        >
-          <Text fontWeight="bold">T</Text>
-        </Box>
-        <Box
-          onClick={setSubtitle}
-          background={(data === 'subtitle' && 'purple.400') || 'purple.200'}
-          cursor="pointer"
-          px={4}
-          py={2}
-          _hover={{ background: 'purple.400' }}
-          borderRadius="9999px"
-        >
-          <Text fontWeight="bold">S</Text>
-        </Box>
-        <Box
-          onClick={setParagraph}
-          background={(data === 'paragraph' && 'purple.400') || 'purple.200'}
-          cursor="pointer"
-          px={4}
-          py={2}
-          _hover={{ background: 'purple.400' }}
-          borderRadius="9999px"
-        >
-          <Text fontWeight="bold">P</Text>
-        </Box>
-        <Box
-          onClick={setPhoto}
-          background={(data === 'photo' && 'purple.400') || 'purple.200'}
-          cursor="pointer"
-          px={4}
-          py={2}
-          _hover={{ background: 'purple.400' }}
-          borderRadius="9999px"
-        >
-          <Text fontWeight="bold">I</Text>
-        </Box>
-        <Box
-          onClick={setCode}
-          background={(data === 'code' && 'purple.400') || 'purple.200'}
-          cursor="pointer"
-          px={4}
-          py={2}
-          _hover={{ background: 'purple.400' }}
-          borderRadius="9999px"
-        >
-          <Text fontWeight="bold">{`</>`}</Text>
-        </Box>
-      </Flex>
-      <Box autoFocus height="80vh" overflowX="hidden" position="relative">
-        {linesJump.map((numb) => (
+      <Box autoFocus height="80vh" overflowX="hidden">
+        {lineJump.map((numb) => (
           <InfoNotes
             key={numb}
+            numb={numb}
+            lineJump={lineJump}
             setLinesJump={setLinesJump}
-            style={data}
-            setStyle={setData}
           />
         ))}
       </Box>
--- components/notes/notes/writer/info/index.js
+++ components/notes/notes/writer/info/index.js
@@ -1,8 +1,9 @@
-import { Box, Heading, Text } from '@chakra-ui/layout';
+import { Box, Divider, Flex, Heading, Text } from '@chakra-ui/layout';
 import { Textarea } from '@chakra-ui/textarea';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import Trash from '../../../../../public/trash';
 
-export const InfoNotes = ({ setLinesJump, style, setStyle }) => {
+export const InfoNotes = ({ setLinesJump, numb, lineJump }) => {
   const [data, setData] = useState([]);
 
   const setValueOfData = (e) => {
@@ -14,19 +15,102 @@ export const InfoNotes = ({ setLinesJump, style, setStyle }) => {
       e.preventDefault() & setLinesJump((cat) => [...cat, cat + 1]);
   };
 
+  const [valueOfStyle, setValueOfStyle] = useState('paragraph');
+
+  const setTitle = () => {
+    if (valueOfStyle === 'title') {
+      setValueOfStyle('paragraph');
+    } else {
+      setValueOfStyle('title');
+    }
+
+    if (menu === true) {
+      setMenu(!true);
+      setValue(!true);
+    }
+  };
+
+  const setSubtitle = () => {
+    if (valueOfStyle === 'subtitle') {
+      setValueOfStyle('paragraph');
+    } else {
+      setValueOfStyle('subtitle');
+    }
+
+    if (menu === true) {
+      setMenu(!true);
+      setValue(!true);
+    }
+  };
+
+  const setParagraph = () => {
+    if (valueOfStyle === 'paragraph') {
+      setValueOfStyle('paragraph');
+    } else {
+      setValueOfStyle('paragraph');
+    }
+
+    if (menu === true) {
+      setMenu(!true);
+      setValue(!true);
+    }
+  };
+
+  // change operator
+
+  let timer;
+
+  const [value, setValue] = useState(false);
+
+  const changeOperator = (e) => {
+    clearTimeout(timer);
+    if (e.detail === 1) {
+      timer = setTimeout(onclick, 200);
+    } else if (e.detail === 2) {
+      setValue(true);
+    }
+  };
+
+  const [menu, setMenu] = useState(false);
+
+  const closePopup = () => {
+    setValue(!true);
+    setMenu(!true);
+  };
+
+  const openMenu = () => {
+    if (menu === false) {
+      setMenu(!false);
+    }
+  };
+
+  // delete component
+
+  const deleteComponent = (e) => {
+    (e.key === 'Backspace') & (data.length === 0) && filterComponent(numb);
+  };
+
+  const filterComponent = (numb) => {
+    setLinesJump((item) => item.filter((item) => item !== numb));
+  };
+
+  // last
+
   return (
     <>
-      {style === 'title' && (
+      {valueOfStyle === 'title' && (
         <Box
           maxWidth="full"
           wordBreak="break-word"
           lineHeight="normal"
-          fontSize="l"
           color="transparent"
           position="relative"
+          fontSize="3xl"
+          mb="10px"
         >
           {data}
           <Textarea
+            onClick={changeOperator}
             resize="none"
             height="full"
             overflowY="scroll"
@@ -34,7 +118,10 @@ export const InfoNotes = ({ setLinesJump, style, setStyle }) => {
             type="text"
             value={data}
             onChange={setValueOfData}
-            onKeyPress={addLineJump}
+            onKeyPress={(e) => {
+              addLineJump(e);
+              deleteComponent(e);
+            }}
             caretColor="#fff"
             position="absolute"
             top="0"
@@ -42,12 +129,13 @@ export const InfoNotes = ({ setLinesJump, style, setStyle }) => {
             p={0}
             autoFocus
             lineHeight="normal"
-            fontSize="l"
-            color="blue.500"
+            fontSize="3xl"
+            color="black"
+            mb="10px"
           />
         </Box>
       )}
-      {style === 'paragraph' && (
+      {valueOfStyle === 'paragraph' && (
         <Box
           maxWidth="full"
           wordBreak="break-word"
@@ -58,6 +146,7 @@ export const InfoNotes = ({ setLinesJump, style, setStyle }) => {
         >
           {data}
           <Textarea
+            onClick={changeOperator}
             resize="none"
             height="full"
             overflowY="scroll"
@@ -66,6 +155,7 @@ export const InfoNotes = ({ setLinesJump, style, setStyle }) => {
             value={data}
             onChange={setValueOfData}
             onKeyPress={addLineJump}
+            onKeyDown={deleteComponent}
             caretColor="#fff"
             position="absolute"
             top="0"
@@ -74,9 +164,131 @@ export const InfoNotes = ({ setLinesJump, style, setStyle }) => {
             autoFocus
             lineHeight="normal"
             fontSize="l"
-            color="red.500"
+            color="black"
           />
         </Box>
+      )}
+      {valueOfStyle === 'subtitle' && (
+        <Box
+          maxWidth="full"
+          wordBreak="break-word"
+          lineHeight="normal"
+          fontSize="xl"
+          color="transparent"
+          position="relative"
+        >
+          {data}
+          <Textarea
+            onClick={changeOperator}
+            resize="none"
+            height="full"
+            overflowY="scroll"
+            variant="unstyled"
+            type="text"
+            value={data}
+            onChange={setValueOfData}
+            onKeyPress={(e) => {
+              addLineJump(e);
+              deleteComponent(e);
+            }}
+            caretColor="#fff"
+            position="absolute"
+            top="0"
+            left="0"
+            p={0}
+            autoFocus
+            lineHeight="normal"
+            fontSize="xl"
+            color="black"
+          />
+        </Box>
+      )}
+
+      {/* controls */}
+
+      {value === true && menu !== true && (
+        <>
+          <Box
+            display="inline-block"
+            p={4}
+            background="#fff"
+            boxShadow="md"
+            position="absolute"
+            zIndex="9999"
+          >
+            <Flex gridGap={4}>
+              <a onClick={openMenu}>
+                <Box cursor="pointer">Turn into</Box>
+              </a>
+              <Box>
+                <Divider orientation="vertical" borderColor="purple.400" />
+              </Box>
+              <Box cursor="pointer">
+                <Trash />
+              </Box>
+            </Flex>
+          </Box>
+        </>
+      )}
+      {(value === true || menu === true) && (
+        <a onClick={closePopup}>
+          <Box
+            position="absolute"
+            top="0"
+            background="transparent"
+            height="80vh"
+            width="80vw"
+            zIndex="9997"
+          ></Box>
+        </a>
+      )}
+
+      {menu === true && (
+        <Flex
+          position="absolute"
+          background="#fff"
+          boxShadow="0px 0px 10px rgba(0, 0, 0, 0.2)"
+          flexDirection="column"
+          zIndex="9998"
+        >
+          <Box
+            onClick={setTitle}
+            cursor="pointer"
+            px={4}
+            py={1}
+            _hover={{ background: 'gray.50' }}
+          >
+            <Heading fontWeight="bold" fontSize="3xl">
+              Title
+            </Heading>
+          </Box>
+          <Box>
+            <Divider />
+          </Box>
+          <Box
+            onClick={setSubtitle}
+            cursor="pointer"
+            px={4}
+            py={2}
+            _hover={{ background: 'gray.50' }}
+          >
+            <Text fontWeight="bold" fontSize="xl">
+              Subtitle
+            </Text>
+          </Box>
+          <Divider />
+          <Box
+            onClick={setParagraph}
+            cursor="pointer"
+            px={4}
+            py={2}
+            _hover={{ background: 'gray.50' }}
+          >
+            <Text fontWeight="bold" fontSize="l">
+              Paragraph
+            </Text>
+          </Box>
+        </Flex>
       )}
     </>
   );
